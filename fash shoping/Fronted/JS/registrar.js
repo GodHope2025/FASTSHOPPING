5fc2d812799a09bd93e2d62b2673b96244b084a3
AldoVlad <correo-de-aldo@gmail.com>
2026-05-14 11:54:16 -0500

    fastshopping-production.up.railway.app

--- fash shoping/Fronted/JS/registrar.js
+++ fash shoping/Fronted/JS/registrar.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             // Enviamos los datos a la ruta que creamos en el backend
-            const respuesta = await fetch('http://localhost:3000/api/registrar', {
+            const respuesta = await fetch('fastshopping-production.up.railway.app', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
